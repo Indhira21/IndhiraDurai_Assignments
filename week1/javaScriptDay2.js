834a1e1e0d944a1128dd7742ae95adfb0320a887
Indhira21 <104789289+Indhira21@users.noreply.github.com>
2026-07-08 10:46:23 +0530

    Add files via upload

---
+++ week1/javaScriptDay2.js
@@ -0,0 +1,16 @@
+let userName = "IndhiraDurai";
+let organisation = "HCL Technologies Limited";
+let ContactNumber = 9087654321;
+let knowsAutomation = true;
+let usesPlaywright;
+
+console.log("User Name:", userName);
+console.log(typeof userName);
+console.log("Organisation:", organisation);
+console.log(typeof organisation);
+console.log("Contact Number:", ContactNumber);
+console.log(typeof ContactNumber);
+console.log("Knows Automation:", knowsAutomation);
+console.log(typeof knowsAutomation);
+console.log("Uses Playwright:", usesPlaywright);
+console.log(typeof usesPlaywright);
---
+++ week1/javaScriptWeek1_CA.js
@@ -0,0 +1,16 @@
+let firstName = "IndhiraDurai";
+let companyName = "HCL Technologies Limited";
+let mobileNumber = 9087654321;
+let isAutomation = true;
+let hasPlaywright;
+
+console.log("First Name:", firstName);
+console.log(typeof firstName);
+console.log("Company Name:", companyName);
+console.log(typeof companyName);
+console.log("Mobile Number:", mobileNumber);
+console.log(typeof mobileNumber);
+console.log("Is Automation:", isAutomation);
+console.log(typeof isAutomation);
+console.log("Has Playwright:", hasPlaywright);
+console.log(typeof hasPlaywright);
